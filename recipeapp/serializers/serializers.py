55e572a3883ee046dcff7fdbb66127f16d306d30
Amukozoberit <mwasheb@gmail.com>
2021-10-14 08:46:27 +0300

    created serializer

---
+++ recipeapp/serializers/serializers.py
@@ -0,0 +1,52 @@
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from star_ratings.models import Rating
+from recipeapp.models.models import Direction, Ingredient, Recipe
+
+
+# Serializers define the API representation.
+class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = serializers.SerializerMethodField()
+    directions = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_ingredients(model):
+        res = []
+        for ing in model.ingredient_set.all():
+            res.append({'name': ing.ingredient.name, 'quantity': ing.quantity, 'index': ing.index})
+        return res
+
+    @staticmethod
+    def get_directions(model):
+        res = []
+        for direction in model.direction_set.all():
+            res.append({'text': direction.text, 'index': direction.index})
+        return res
+    class Meta:
+        model = Recipe
+        fields = (
+            'created_at', 'user', 'name','image',  'summary', 'prep_time', 'cook_time', 'servings', 'calories',
+            'is_featured',  'ingredients', 'directions', 
+        )
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__'
+
+class DirectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Direction
+        fields = '__all__'
+
+
+
+
+       
