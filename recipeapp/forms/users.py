158e016ea86687d5e8746e11285728b7a662c8b9
Amukozoberit <mwasheb@gmail.com>
2021-10-14 08:48:35 +0300

    created users view

--- recipeapp/forms/users.py
+++ recipeapp/forms/users.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 
-from recipeapp.models import UserProfile, IngredientName, Recipe, Ingredient
+from recipeapp.models.models import UserProfile, IngredientName, Recipe, Ingredient
 
 class SaveUserProfileForm(forms.ModelForm):
 
